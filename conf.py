430aae4656d840c7eaa435958c5f090161a087c4
fcomitani <federico.comitani@gmail.com>
2022-01-20 18:19:55 -0500

    restructuring

--- conf.py
+++ conf.py
@@ -64,7 +64,7 @@ html_theme = 'press'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 def skip(app, what, name, obj, would_skip, options):
     if name == "__init__":
